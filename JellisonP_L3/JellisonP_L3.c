f5dd9e14967e8cd585fdd1cafcaf1187d5f8f8b8
Payton Jellison <pcjellison@ksu.edu>
2017-09-06 20:54:33 -0500

    DONE

--- JellisonP_L3/JellisonP_L3.c
+++ JellisonP_L3/JellisonP_L3.c
@@ -1,6 +1,42 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-void main()
+void UpperCase(char*);
+void Reversed(char*);
+
+int main()
 {
+	char firstName [11] = "John Smith";
+	char secondName[11] = "Mary Cohen";
+	char thirdName[14] = "Carl Williams";
 
+	UpperCase(firstName);
+	UpperCase(secondName);
+	UpperCase(thirdName);
+
+	Reversed(firstName);
+	Reversed(secondName);
+	Reversed(thirdName);
+}
+
+void UpperCase(char* name)
+{
+	for (int i = 0; i < strlen(name); i++)
+	{
+		*(name + i) = toupper(*(name + i));
+	}
+}
+
+void Reversed (char* name)
+{
+	char temp[14];
+	int count = 0;
+	for (int i = (strlen(name)-1); i >= 0; i--)
+	{
+		temp[count] = *(name + i);
+		count++;
+	}
+	temp[count] = '\0';
+	printf("%s \n", temp);
 }
